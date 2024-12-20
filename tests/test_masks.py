bcd2f48eae7c08d9cf4c8a023d07d6fdefd442bf
Svetlana Khmel <svetlankaaa88@mail.ru>
2024-12-20 03:00:49 +0300

    htmlcov

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -5,8 +5,11 @@ def test_get_mask_card_number_():
     assert get_mask_card_number("7000792289606361") == "7000 79** **** 6361"
     assert get_mask_card_number("7003230792289606361") == 'Номер карты введен неверно.'
     assert get_mask_card_number("") == 'Номер карты введен неверно.'
+    assert get_mask_card_number("vnvnvnvnvn") == 'Номер карты введен неверно.'
+
 
 def test_get_mask_account():
     assert get_mask_account("73654108430135874305") == "**4305"
     assert get_mask_account("708430135874305")== "Номер счета введен неверно."
     assert get_mask_account("")== "Номер счета введен неверно."
+    assert get_mask_account("vnbvnvn") == "Номер счета введен неверно."
