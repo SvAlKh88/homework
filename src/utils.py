6b37f4f30e73f5ddb0588d97072c3f17df71ee97
Svetlana Khmel <svetlankaaa88@mail.ru>
2025-01-24 21:15:30 +0300

    init utils

---
+++ src/utils.py
@@ -0,0 +1,19 @@
+import json
+
+def returns_list_of_dictionariesdict(path: str) -> list[dict] :
+    ''' Принимает на вход путь до JSON-файла и 
+    возвращает список словарей с данными о финансовых транзакциях '''
+    try:
+        with open(path) as f:
+            try:
+                financial_transaction_data = json.load(f)
+            except json.JSONDecodeError:
+                financial_transaction_data = []
+    except FileNotFoundError:
+        financial_transaction_data = []
+    return financial_transaction_data
+
+
+if __name__ == "__main__": # pragma:no cover
+    list_of_dict = returns_list_of_dictionariesdict("../data/operations.json")
+    print(list_of_dict)
