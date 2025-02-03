1aa8ff0db22d17bef777478b9f4ca991b1f39c36
Svetlana Khmel <svetlankaaa88@mail.ru>
2025-02-03 22:51:11 +0300

    logger utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,19 +1,37 @@
 import json
+
 from typing import (
     Any
 )
+import logging
+
+
+logger = logging.getLogger("utils")
+file_handler = logging.FileHandler('../logs/utils.log',"w")
+file_formatter = logging.Formatter("%(asctime)s %(filename)s %(levelname)s: %(message)s)")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
 
 
 def returns_list_of_dictionaries(path: str) -> Any:
-    """Принимает на вход путь до JSON-файла и
+    """ Принимает на вход путь до JSON-файла и
     возвращает список словарей с данными о финансовых транзакциях"""
+
+    logger.info(" Записываем список словарей с данными о финансовых транзакциях ")
+
+
     try:
         with open(path, encoding="utf-8") as f:
             try:
                 financial_transaction_data = json.load(f)
+                logger.info(" Фаил успешно записан ")
             except json.JSONDecodeError:
+                logger.error(" Ошибка декодировки файла ")
                 financial_transaction_data = []
     except FileNotFoundError:
+
+        logger.error(" Файл не найден ")
         financial_transaction_data = []
     return financial_transaction_data
 
