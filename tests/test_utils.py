ee27a4015a6ed7b4bf4cf1c000daec2079d4d36f
Svetlana Khmel <svetlankaaa88@mail.ru>
2025-02-22 17:49:18 +0300

    test_string_search

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,11 +1,12 @@
 import json
+import pytest
 from unittest.mock import (
     mock_open,
     patch
 )
 
 from src.utils import (
-    returns_list_of_dictionaries
+    returns_list_of_dictionaries, string_search
 )
 
 
@@ -52,3 +53,45 @@ def test_returns_list_of_dictionaries(mock_get):
         }
     ]
     mock_get.assert_called()
+
+
+list_of_financial_transaction = [
+    {'id': 441945886, 'state': 'EXECUTED', 'date': '2019-08-26T10:50:58.294041',
+     'operationAmount': {'amount': '31957.58', 'currency': {'name': 'руб.', 'code': 'RUB'}},
+     'description': 'Перевод организации', 'from': 'Maestro 1596837868705199', 'to': 'Счет 64686473678894779589'},
+    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364',
+     'operationAmount': {'amount': '8221.37', 'currency': {'name': 'USD', 'code': 'USD'}},
+     'description': 'Открытие счета', 'from': 'MasterCard 7158300734726758', 'to': 'Счет 35383033474447895560'},
+    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572',
+     'operationAmount': {'amount': '9824.07', 'currency': {'name': 'USD', 'code': 'USD'}},
+     'description': 'Перевод организации', 'from': 'Счет 75106830613657916952', 'to': 'Счет 11776614605963066702'},
+]
+
+
+@pytest.mark.parametrize(
+    "list_of_financial_transaction, my_string, expected",
+    [
+        (list_of_financial_transaction, 'перевод',
+         [{'id': 441945886, 'state': 'EXECUTED', 'date': '2019-08-26T10:50:58.294041',
+           'operationAmount': {'amount': '31957.58', 'currency': {'name': 'руб.', 'code': 'RUB'}},
+           'description': 'Перевод организации', 'from': 'Maestro 1596837868705199', 'to': 'Счет 64686473678894779589'},
+          {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572',
+           'operationAmount': {'amount': '9824.07', 'currency': {'name': 'USD', 'code': 'USD'}},
+           'description': 'Перевод организации', 'from': 'Счет 75106830613657916952',
+           'to': 'Счет 11776614605963066702'},
+          ]),
+        (list_of_financial_transaction, 'ПЕРевод',
+         [{'id': 441945886, 'state': 'EXECUTED', 'date': '2019-08-26T10:50:58.294041',
+           'operationAmount': {'amount': '31957.58', 'currency': {'name': 'руб.', 'code': 'RUB'}},
+           'description': 'Перевод организации', 'from': 'Maestro 1596837868705199', 'to': 'Счет 64686473678894779589'},
+          {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572',
+           'operationAmount': {'amount': '9824.07', 'currency': {'name': 'USD', 'code': 'USD'}},
+           'description': 'Перевод организации', 'from': 'Счет 75106830613657916952',
+           'to': 'Счет 11776614605963066702'},
+          ]),
+        (list_of_financial_transaction, 'неперевод', []),
+        ([{'id': 441945886, 'state': 'EXECUTED', 'date': '2019-08-26T10:50:58.294041'},
+          {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}], 'открытие', [])
+    ])
+def test_string_search(list_of_financial_transaction, my_string, expected):
+    assert string_search(list_of_financial_transaction, my_string) == expected
